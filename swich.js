ac3739c672c9dad5aae60df534b0aa81894a6f0e
VikMelanchenko <63492650+VikMelanchenko@users.noreply.github.com>
2020-11-04 01:01:06 +0200

    добавляет изменения в скрипт (2й вариант)

--- swich.js
+++ swich.js
@@ -15,17 +15,48 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-startBtn.addEventListener("click", swichStart);
-stopBtn.addEventListener("click", swichStop);
+// вариант 2
+let intervalId = null;
 
-function swichStart() {
-  startBtn.setAttribute("disabled", true);
-  colorSwich = setInterval(() => {
-    let color = randomIntegerFromInterval(0, colors.length - 1);
-    body.style.background = colors[color];
-  }, 1000);
-}
+const swicher = {
+  intervalId: null,
+  isActive: false,
 
-function swichStop() {
-  clearInterval(colorSwich);
-}
+  start() {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+    startBtn.disabled = true;
+    this.intervalId = setInterval(() => {
+      const color = randomIntegerFromInterval(0, 5);
+      body.style.background = colors[color];
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    startBtn.disabled = false;
+  },
+};
+
+startBtn.addEventListener("click", () => {
+  swicher.start();
+});
+stopBtn.addEventListener("click", () => {
+  swicher.stop();
+});
+
+// вариант 1й
+// function swichStart() {
+//   startBtn.setAttribute("disabled", true);
+//   colorSwich = setInterval(() => {
+//     const color = randomIntegerFromInterval(0, colors.length - 1);
+//     body.style.background = colors[color];
+//   }, 1000);
+// }
+
+// function swichStop() {
+//   clearInterval(colorSwich);
+// }
